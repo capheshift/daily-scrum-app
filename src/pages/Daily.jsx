529fdb482909f45e74132f135c9073206d692e4d
Tam Pham <tampham47@live.com>
2015-08-18 23:20:45 +0700

    add daily element

--- src/pages/Daily.jsx
+++ src/pages/Daily.jsx
@@ -19,7 +19,72 @@ var DailyPage = React.createClass({
     return (
       <div className="container">
         <h4>DAILY <small>The more you plan, the better you success !</small></h4>
-        <p>TODAY</p>
+
+        <div className="day-block">
+          <p>TODAY</p>
+          <ul className="daily-list">
+            <li className="daily-item">
+              <div className="input-group">
+                <span className="input-group-addon"> <input type="checkbox" /></span>
+                <input className="form-control" id="prependedcheckbox"
+                  name="prependedcheckbox" placeholder="your task" type="text"
+                  value="Nếu biết tình như thế, chẳng lớn lên làm gì" />
+              </div>
+            </li>
+            <li className="daily-item">
+              <div className="input-group">
+                <span className="input-group-addon"> <input type="checkbox" /></span>
+                <input className="form-control" id="prependedcheckbox"
+                  name="prependedcheckbox" placeholder="your task" type="text"
+                  value="Thuở còn thơ ngày 3 cữ là thường, tôi lai rai qua từng chai lớn nhỏ" />
+              </div>
+            </li>
+            <li className="daily-item">
+              <button className="btn btn-sm btn-default">+ new task</button>
+            </li>
+          </ul>
+        </div>
+
+        <div className="day-block">
+          <p>TOMORROW</p>
+          <ul className="daily-list">
+            <li className="daily-item">
+              <div className="input-group">
+                <span className="input-group-addon"> <input type="checkbox" /></span>
+                <input className="form-control" id="prependedcheckbox"
+                  name="prependedcheckbox" placeholder="your task" type="text"
+                  value="Ai bảo say sưa là khổ" />
+              </div>
+            </li>
+            <li className="daily-item">
+              <div className="input-group">
+                <span className="input-group-addon"> <input type="checkbox" /></span>
+                <input className="form-control" id="prependedcheckbox"
+                  name="prependedcheckbox" placeholder="your task" type="text"
+                  value="Tôi mơ màng nghe men vút lên cao," />
+              </div>
+            </li>
+            <li className="daily-item">
+              <button className="btn btn-sm btn-default">+ new task</button>
+            </li>
+          </ul>
+        </div>
+
+        <div className="day-block">
+          <p>MAY 5th</p>
+          <ul className="daily-list">
+            <li className="daily-item">
+              <div className="input-group">
+                <span className="input-group-addon"> <input type="checkbox" /></span>
+                <input className="form-control" id="prependedcheckbox"
+                  name="prependedcheckbox" placeholder="your task" type="text" />
+              </div>
+            </li>
+            <li className="daily-item">
+              <button className="btn btn-sm btn-default">+ new task</button>
+            </li>
+          </ul>
+        </div>
 
       </div>
     );

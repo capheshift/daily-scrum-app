ec69dc1ac27bc11ca10e592ad9ff6c594344ef22
Tam Pham <tampham47@live.com>
2015-08-22 04:53:27 +0700

    dynamic add task

--- src/pages/Daily.jsx
+++ src/pages/Daily.jsx
@@ -15,77 +15,90 @@ var DailyPage = React.createClass({
     };
   },
 
+  getInitialState: function() {
+    return {
+      dateList: [{
+        displayName: 'TODAY',
+        value: '20150822'
+      }],
+      taskList: [{
+        date: '2015022',
+        task: 'Nếu biết tình như thế, chẳng lớn lên làm gì',
+        isCompleted: false
+      },
+      {
+        date: '2015022',
+        task: 'Thà như ngày thơ ấu, hai đứa cầm tay đi',
+        isCompleted: false
+      }]
+    }
+  },
+
+  newTaskOnClicked: function(dateItem) {
+    console.log('newTaskOnClicked', dateItem);
+    var newTaskList = this.state.taskList.concat([{
+      date: dateItem.value,
+      isCompleted: false,
+      value: ''
+    }]);
+
+    this.setState({
+      taskList: newTaskList
+    });
+  },
+
+  renderTaskList: function(dateItem) {
+    if (!this.state.taskList.length) {
+      return '';
+    }
+
+    return (
+      <ul className="daily-list">
+        {this.state.taskList.map(function(item, i) {
+          return (
+            <li className="daily-item">
+              <div className="input-group">
+                <span className="input-group-addon"> <input type="checkbox" /></span>
+                <input className="form-control" id="prependedcheckbox"
+                  name="prependedcheckbox" placeholder="your task" type="text"
+                  value={item.task} />
+              </div>
+            </li>
+          )
+        })}
+        <li className="daily-item">
+          <button className="btn btn-sm btn-default"
+            onClick={this.newTaskOnClicked.bind(null, dateItem)}>+ new task</button>
+        </li>
+      </ul>
+    );
+  },
+
+  renderDateList: function() {
+    if (!this.state.dateList.length) {
+      return '';
+    }
+
+    return (
+      <div>
+        {this.state.dateList.map(function(item, i) {
+          return (
+            <div className="day-block">
+              <p>{item.displayName}</p>
+              {this.renderTaskList(item)}
+            </div>
+          )
+        }.bind(this))}
+      </div>
+    );
+  },
+
   render: function() {
     return (
       <div className="">
         <h4>DAILY <small>The more you plan, the better you success !</small></h4>
 
-        <div className="day-block">
-          <p>TODAY</p>
-          <ul className="daily-list">
-            <li className="daily-item">
-              <div className="input-group">
-                <span className="input-group-addon"> <input type="checkbox" /></span>
-                <input className="form-control" id="prependedcheckbox"
-                  name="prependedcheckbox" placeholder="your task" type="text"
-                  value="Nếu biết tình như thế, chẳng lớn lên làm gì" />
-              </div>
-            </li>
-            <li className="daily-item">
-              <div className="input-group">
-                <span className="input-group-addon"> <input type="checkbox" /></span>
-                <input className="form-control" id="prependedcheckbox"
-                  name="prependedcheckbox" placeholder="your task" type="text"
-                  value="Thuở còn thơ ngày 3 cữ là thường, tôi lai rai qua từng chai lớn nhỏ" />
-              </div>
-            </li>
-            <li className="daily-item">
-              <button className="btn btn-sm btn-default">+ new task</button>
-            </li>
-          </ul>
-        </div>
-
-        <div className="day-block">
-          <p>TOMORROW</p>
-          <ul className="daily-list">
-            <li className="daily-item">
-              <div className="input-group">
-                <span className="input-group-addon"> <input type="checkbox" /></span>
-                <input className="form-control" id="prependedcheckbox"
-                  name="prependedcheckbox" placeholder="your task" type="text"
-                  value="Ai bảo say sưa là khổ" />
-              </div>
-            </li>
-            <li className="daily-item">
-              <div className="input-group">
-                <span className="input-group-addon"> <input type="checkbox" /></span>
-                <input className="form-control" id="prependedcheckbox"
-                  name="prependedcheckbox" placeholder="your task" type="text"
-                  value="Tôi mơ màng nghe men vút lên cao," />
-              </div>
-            </li>
-            <li className="daily-item">
-              <button className="btn btn-sm btn-default">+ new task</button>
-            </li>
-          </ul>
-        </div>
-
-        <div className="day-block">
-          <p>MAY 5th</p>
-          <ul className="daily-list">
-            <li className="daily-item">
-              <div className="input-group">
-                <span className="input-group-addon"> <input type="checkbox" /></span>
-                <input className="form-control" id="prependedcheckbox"
-                  name="prependedcheckbox" placeholder="your task" type="text" />
-              </div>
-            </li>
-            <li className="daily-item">
-              <button className="btn btn-sm btn-default">+ new task</button>
-            </li>
-          </ul>
-        </div>
-
+        {this.renderDateList()}
       </div>
     );
   }

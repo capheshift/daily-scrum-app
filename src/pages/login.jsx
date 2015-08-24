cf52cd2005e323fa9663d54b939657d6aa45f86f
Tam Pham <tampham47@live.com>
2015-08-24 15:46:31 +0700

    add login action

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -5,6 +5,7 @@
 
 var React = require('react');
 var DefaultLayout = React.createFactory(require('../layouts/Default'));
+// var Auth = require('');
 
 var LoginPage = React.createClass({
   displayName: 'Login Page',

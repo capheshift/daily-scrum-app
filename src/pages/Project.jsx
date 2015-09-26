4db28ea328090231b49fbed17a103778221768cb
Tam Pham <tampham47@live.com>
2015-09-26 15:15:28 +0700

    style project page

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -5,6 +5,7 @@
 
 var React = require('react');
 var DefaultLayout = React.createFactory(require('../layouts/Default'));
+var Select = React.createFactory(require('react-select'));
 
 var ProjectPage = React.createClass({
   displayName: 'Project',
@@ -16,26 +17,83 @@ var ProjectPage = React.createClass({
   },
 
   render: function() {
+    var projectOptions = [
+      { value: 'vib', label: 'VIB' },
+      { value: 'nafoods', label: 'Nafoods' },
+      { value: 'daily-scrum', label: 'Daily Scrum' }
+    ];
+
     return (
       <div className="row">
         <div className="col-sm-12">
           <h4>PROJECT</h4>
         </div>
 
-        <div className="col-md-6">
-          <div className="input-group">
-            <span className="input-group-addon"> <input type="checkbox" /></span>
-            <input className="form-control" id="prependedcheckbox"
-              name="prependedcheckbox" placeholder="your task" type="text" />
-          </div>
+        <div className="col-sm-6">
+          <table className="table table-striped">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Project Name</th>
+                <th>Leader</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>VIB</td>
+                <td>Otto</td>
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>Nafoods</td>
+                <td>Thornton</td>
+              </tr>
+              <tr>
+                <th scope="row">3</th>
+                <td>Daily Scrum</td>
+                <td>the Bird</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
 
-        <div className="col-sm-12">
-          <ul className="project-list">
-            <li className="project-item">VIB</li>
-            <li className="project-item">Nafoods</li>
-            <li className="project-item">Daily Scrum</li>
-          </ul>
+        <div className="col-sm-6">
+          <form className="form-horizontal">
+            <fieldset>
+              <div className="form-group">
+                <label className="col-sm-3 control-label" for="textinput">Project</label>
+                <div className="col-sm-9">
+                  <input id="textinput" name="textinput" type="text" placeholder="name of project"
+                    className="form-control input-md" />
+                </div>
+              </div>
+
+              <div className="form-group">
+                <label className="col-sm-3 control-label" for="textinput">Leader</label>
+                <div className="col-sm-9">
+                  <Select name="form-field-name" value="nafoods" clearable={false}
+                    options={projectOptions} onChange={this.onSelectChanged} />
+                </div>
+              </div>
+
+              <div className="form-group">
+                <label className="col-sm-3 control-label" for="textinput">Members</label>
+                <div className="col-sm-9">
+                  <Select name="form-field-name" value="nafoods" clearable={false}
+                    options={projectOptions} onChange={this.onSelectChanged} />
+                </div>
+              </div>
+
+              <div className="form-group">
+                <label className="col-sm-3 control-label" for="button1id"></label>
+                <div className="col-md-9">
+                  <button id="button1id" name="button1id" className="btn btn-success pull-right">Create project</button>
+                </div>
+              </div>
+
+            </fieldset>
+          </form>
         </div>
       </div>
     );

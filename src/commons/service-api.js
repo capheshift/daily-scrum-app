407f8ec6f8145aa9875deeab2a96ab0e5312bdd3
Tam Pham <tampham47@live.com>
2015-10-17 21:43:13 +0700

    Try to make simple of request

--- src/commons/service-api.js
+++ src/commons/service-api.js
@@ -1,7 +1,8 @@
 'use strict';
 
-var Promise = require('promise');
 var $ = require('jquery');
+var _ = require('lodash');
+var Promise = require('promise');
 var config = require('../config');
 var apiList, result = {};
 
@@ -12,25 +13,25 @@ apiList = [
   { nspace: 'UserApis', name: 'login', path: '/users/login', method: 'POST' },
   { nspace: 'UserApis', name: 'logout', path: '/users/logout', method: 'POST' },
   { nspace: 'UserApis', name: 'getById', path: '/users/${_id}/detail', method: 'GET' },
-  { nspace: 'UserApis', name: 'find', path: '/users/find', method: 'GET' },
+  { nspace: 'UserApis', name: 'find', path: '/users/find?q=${q}&l=${l}', method: 'GET' },
   { nspace: 'UserApis', name: 'all', path: '/users/all', method: 'GET' },
   // task
   { nspace: 'TaskApis', name: 'all', path: '/tasks/all', method: 'GET' },
-  { nspace: 'TaskApis', name: 'find', path: '/tasks/find', method: 'GET' },
+  { nspace: 'TaskApis', name: 'find', path: '/tasks/find?q=${q}&l=${l}', method: 'GET' },
   { nspace: 'TaskApis', name: 'detail', path: '/tasks/${_id}/all', method: 'GET' },
   { nspace: 'TaskApis', name: 'create', path: '/tasks/', method: 'POST' },
   { nspace: 'TaskApis', name: 'update', path: '/tasks/${_id}', method: 'PUT' },
   { nspace: 'TaskApis', name: 'delete', path: '/tasks/${_id}', method: 'DELETE' },
   // project
   { nspace: 'ProjectApis', name: 'all', path: '/projects/all', method: 'GET' },
-  { nspace: 'ProjectApis', name: 'find', path: '/projects/find', method: 'GET' },
+  { nspace: 'ProjectApis', name: 'find', path: '/projects/find?q=${q}&l=${l}', method: 'GET' },
   { nspace: 'ProjectApis', name: 'detail', path: '/projects/${_id}/all', method: 'GET' },
   { nspace: 'ProjectApis', name: 'create', path: '/projects/', method: 'POST' },
   { nspace: 'ProjectApis', name: 'update', path: '/projects/${_id}', method: 'PUT' },
   { nspace: 'ProjectApis', name: 'delete', path: '/projects/${_id}', method: 'DELETE' },
   // user-project
   { nspace: 'UProjectApis', name: 'all', path: '/user-project/all', method: 'GET' },
-  { nspace: 'UProjectApis', name: 'find', path: '/user-project/find', method: 'GET' },
+  { nspace: 'UProjectApis', name: 'find', path: '/user-project/find?q=${q}&l=${l}', method: 'GET' },
   { nspace: 'UProjectApis', name: 'detail', path: '/user-project/${_id}/all', method: 'GET' },
   { nspace: 'UProjectApis', name: 'create', path: '/user-project/', method: 'POST' },
   { nspace: 'UProjectApis', name: 'update', path: '/user-project/${_id}', method: 'PUT' },
@@ -43,8 +44,14 @@ $.each(apiList, function(index, item) {
     result[item.nspace] = {};
   }
 
-  result[item.nspace][item.name] = function(data) {
+  /**
+   * wrapper to make request easier
+   * @param  data   [that data wanna passed into body of request]
+   * @param  params [that data wanna passed into url]
+   */
+  result[item.nspace][item.name] = function(data, params) {
     return new Promise(function(resolve, reject) {
+      var realPath = '';
       var token = window.localStorage.getItem('token');
       var headers = {
         'Content-Type': 'application/json'
@@ -54,8 +61,19 @@ $.each(apiList, function(index, item) {
         headers.Authorization = 'Bearer ' + token;
       }
 
+      if (item.name === 'find') {
+        realPath = _.template(item.path)({
+          q: JSON.stringify(params.q),
+          l: JSON.stringify(params.l)
+        });
+      } else {
+        realPath = _.template(item.path)(params);
+      }
+
+      console.log('service', data, params, realPath);
+
       $.ajax({
-        url: config.apiPath + item.path,
+        url: config.apiPath + realPath,
         type: item.method,
         method: item.method,
         data: JSON.stringify(data),

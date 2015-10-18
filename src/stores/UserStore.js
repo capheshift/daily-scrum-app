39eb4555da08b6ca6ad046f89bb91c5d87fff6b7
Tam Pham <tampham47@live.com>
2015-10-18 22:30:10 +0700

    update login, register, logout flow

--- src/stores/UserStore.js
+++ src/stores/UserStore.js
@@ -49,6 +49,21 @@ var UserStore = assign({}, EventEmitter.prototype, {
   rmvListenerOnLoginFail: function(context) {
     this.removeListener(Events.LoginFail, context);
   },
+  //
+  // listener for login
+  addListenerOnLogoutSuccess: function(callback, context) {
+    this.on(Events.LogoutSuccess, callback, context);
+  },
+  rmvListenerOnLogoutSuccess: function(context) {
+    this.removeListener(Events.LogoutSuccess, context);
+  },
+  addListenerOnLogoutFail: function(callback, context) {
+    this.on(Events.LogoutFail, callback, context);
+  },
+  rmvListenerOnLogoutFail: function(context) {
+    this.removeListener(Events.LogoutFail, context);
+  },
+
 
   // functions
   login: function(data) {
@@ -59,6 +74,7 @@ var UserStore = assign({}, EventEmitter.prototype, {
     function(body) {
       // set token into localstorage
       window.localStorage.setItem('token', body.data.token);
+      window.localStorage.setItem('fullName', body.data.fullName);
       this.emit(Events.LoginSuccess, body);
     }.bind(this),
     function(err) {
@@ -68,6 +84,8 @@ var UserStore = assign({}, EventEmitter.prototype, {
 
   logout: function(data) {
     console.log('logout data', data);
+    window.localStorage.clear();
+    this.emit(Events.LogoutSuccess);
   },
 
   register: function(data) {
@@ -78,6 +96,7 @@ var UserStore = assign({}, EventEmitter.prototype, {
       console.log('register', body);
       // set token into localstorage
       window.localStorage.setItem('token', body.data.token);
+      window.localStorage.setItem('fullName', body.data.fullName);
       this.emit(Events.RegisterSuccess, body);
     }.bind(this),
     function(err) {

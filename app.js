06e0e9dafb9f2fddd0f14f456428da13932e27df
Tam Pham <tampham47@live.com>
2015-10-21 00:21:49 +0700

    update apiPath to heroku

--- app.js
+++ app.js
@@ -21645,8 +21645,8 @@
 	var CFG = {
 	  // Paint Area for this application
 	  container: document.getElementById('app'),
-	  // apiPath: 'http://daily-scrum-api.herokuapp.com',
-	  apiPath: 'http://localhost:3000',
+	  apiPath: 'http://daily-scrum-api.herokuapp.com',
+	  // apiPath: 'http://localhost:3000',
 
 	  estimateList: [
 	    { value: '0.5', label: '30 mins' },
